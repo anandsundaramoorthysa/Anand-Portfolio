2aabd9559fdff6859e9c99cd5adc3987867306f2
anandsundaramoorthysa <sanand03072005@gmail.com>
2025-03-25 22:28:48 +0530

    Update

--- src/components/TypedTitle.tsx
+++ src/components/TypedTitle.tsx
@@ -10,7 +10,7 @@ type TitleData = {
 
 const TypedTitle = () => {
   const titles: TitleData[] = [
-    { text: "Front-End & Flutter Developer", icon: <Code className="h-6 w-6" /> },
+    { text: "Developer", icon: <Code className="h-6 w-6" /> },
     { text: "Tech & Finance Enthusiast", icon: <TrendingUp className="h-6 w-6" /> },
     { text: "Passionate Learner", icon: <Lightbulb className="h-6 w-6" /> },
     { text: "Writer", icon: <BookText className="h-6 w-6" /> },
